88476db008954e780c51728c4246c8c0bcfd8bee
Huzaifa-314 <hamim109837@gmail.com>
2024-08-24 00:11:36 +0600

    fix

--- main.js
+++ main.js
@@ -1,17 +1,11 @@
 // addition
-// substraction
-// multiplication
-// division
-// percentage
 // trigonometry
     // 1. sin 
     // 2. cos
     // 3. tan
 // 10 base log
 // natural log
-// power
 // square root
-// factorial
 // fraction
 
 function addition(a,b){
@@ -98,6 +92,12 @@ function addition(a,b){
 
 
 
+// substraction
+// multiplication
+// division
+// percentage
+// factorial
+// power
 function multiplication(a, b){
     return a*b;
 }
