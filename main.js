74964ae4c09452e0aef294e3bb920858a92a2437
Huzaifa-314 <hamim109837@gmail.com>
2024-08-23 23:31:54 +0600

    list of all necessary function

---
+++ main.js
@@ -0,0 +1,14 @@
+// addition
+// substraction
+// multiplication
+// division
+// percentage
+// trigonometry
+    // 1. sin 
+    // 2. cos
+    // 3. tan
+// 10 base log
+// natural log
+// power
+// square root
+// factorial
