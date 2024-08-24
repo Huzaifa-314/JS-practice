c371deae67095548dc04dcbcdec83a0f6df6e4d0
alimulhuq <alimulhuq3179@gmail.com>
2024-08-25 01:17:04 +0600

    all keys feature is added

--- scientific_calculator.js
+++ scientific_calculator.js
@@ -1,58 +1,19 @@
-// 10 base log
-
-// Sine
-function sin(a){
-    return Math.sin(a);    
-}
-
-// Cosine
-function cos(a){
-    return Math.cos(a);
-}
-
-// tangent
-function tan(a){
-    return Math.tan(a);
-}
-
-// squer root
-function sqrt(a){
-    return Math.sqrt(a);
-}
-
-// natural log
-function ln(a){
-    return Math.log(a);
-}
-
-// power
-function power(a, b){
-    return Math.pow(a, b);
-}
-
-// Main functionality 
-// ====================================
-// ====================================
-// ====================================
-// ====================================
-
-let numberPads = document.querySelectorAll(".btn");
-let display = document.querySelector("#head");
-let logBtn = document.querySelector(".logarithm");
-let sineBtn = document.querySelector(".sine");
-let cosineBtn = document.querySelector(".cosine");
-let tangentBtn = document.querySelector(".tangent");
-let squreBtn = document.querySelector(".squre");
-let rootBtn = document.querySelector(".root");
+let numberPads = document.querySelectorAll(".btn"); 
+let display = document.querySelector("#head"); 
+let logBtn = document.querySelector(".logarithm"); 
+let sineBtn = document.querySelector(".sine"); 
+let cosineBtn = document.querySelector(".cosine"); 
+let tangentBtn = document.querySelector(".tangent"); 
+let squreBtn = document.querySelector(".squre"); 
+let rootBtn = document.querySelector(".root"); 
 let powerBtn = document.querySelector(".power");
-let braketBtn1 = document.querySelector(".braket1");
-let braketBtn2 = document.querySelector(".braket2");
-let equalBtn = document.querySelector(".equal");
-let numberBtns = document.querySelectorAll(".number");
-let clearBtn = document.querySelector(".clear");
-let backSpaceBtn = document.querySelector(".backspace");
-let binaryOps = document.querySelectorAll(".binary_op");
-
+let braketBtn1 = document.querySelector(".braket1"); 
+let braketBtn2 = document.querySelector(".braket2"); 
+let equalBtn = document.querySelector(".equal"); 
+let numberBtns = document.querySelectorAll(".number"); 
+let clearBtn = document.querySelector(".clear"); 
+let backSpaceBtn = document.querySelector(".backspace"); 
+let binaryOps = document.querySelectorAll(".binary_op"); 
 
 // catching error
 function calculate(expression) {
@@ -62,6 +23,7 @@ function calculate(expression) {
         return "Math Error";
     }
 }
+
 numberBtns.forEach((numberBtn) => {
     numberBtn.addEventListener("click", () => {
        display.value += numberBtn.value; 
@@ -71,6 +33,18 @@ binaryOps.forEach((numberBtn) => {
     numberBtn.addEventListener("click", () => {
        display.value += numberBtn.value; 
     });
+});
+
+powerBtn.addEventListener("click", () => {
+    let x = prompt("Enter Base Number : ");
+    let y = prompt("Enter Power Number : ");
+    display.value = Math.pow(x, y);
+});
+squreBtn.addEventListener("click", () => {
+    display.value = Math.pow(display.value, 2);
+});
+rootBtn.addEventListener("click", () => {
+    display.value = Math.sqrt(display.value);
 });
 
 sineBtn.addEventListener("click", () => {
