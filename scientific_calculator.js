13050b73de6906b698833941e945d52345f08d4f
Huzaifa-314 <hamim109837@gmail.com>
2024-08-26 00:07:23 +0600

    sin, cos, tan: making it dynamic

--- scientific_calculator.js
+++ scientific_calculator.js
@@ -1,7 +1,7 @@
 let numberPads = document.querySelectorAll(".btn"); 
 let display = document.querySelector("#head"); 
 let logBtn = document.querySelector(".logarithm"); 
-let sineBtn = document.querySelector(".sine"); 
+let sineBtn = document.querySelector(".sine");
 let cosineBtn = document.querySelector(".cosine"); 
 let tangentBtn = document.querySelector(".tangent"); 
 let squreBtn = document.querySelector(".squre"); 
@@ -15,6 +15,9 @@ let clearBtn = document.querySelector(".clear");
 let backSpaceBtn = document.querySelector(".backspace"); 
 let binaryOps = document.querySelectorAll(".binary_op"); 
 
+let things_to_replace = new Set();
+
+
 // catching error
 function calculate(expression) {
     try {
@@ -22,6 +25,16 @@ function calculate(expression) {
     } catch (error) {
         return "Math Error";
     }
+}
+
+// expression finalize function
+function finalize(expression){
+    things_to_replace.forEach((thing) =>{
+        
+
+    });
+    
+    return expression;
 }
 
 numberBtns.forEach((numberBtn) => {
@@ -34,6 +47,25 @@ binaryOps.forEach((numberBtn) => {
        display.value += numberBtn.value; 
     });
 });
+braketBtn1.addEventListener("click", () => {
+    display.value += braketBtn1.value;
+});
+braketBtn2.addEventListener("click", () => {
+    display.value += braketBtn2.value;
+});
+
+
+
+
+sineBtn.addEventListener("click", () => {
+    things_to_replace.add("sin");
+    display.value += sineBtn.value;
+});
+
+
+
+
+
 
 powerBtn.addEventListener("click", () => {
     let x = prompt("Enter Base Number : ");
@@ -46,37 +78,34 @@ squreBtn.addEventListener("click", () => {
 rootBtn.addEventListener("click", () => {
     display.value = Math.sqrt(display.value);
 });
-
-sineBtn.addEventListener("click", () => {
-    display.value = Math.sin(display.value);
-    // display.value += sineBtn.value;
-});
 cosineBtn.addEventListener("click", () => {
+    things_to_replace.add("cos");
     display.value = Math.cos(display.value);
     // display.value += cosineBtn.value;
 });
 tangentBtn.addEventListener("click", () => {
+    things_to_replace.add("tan");
     display.value = Math.tan(display.value);
     // display.value += tangentBtn.value;
 });
 logBtn.addEventListener("click", () => {
+    things_to_replace.add("log");
     display.value = Math.log(display.value);
     // display.value += logBtn.value;
 });
 
-braketBtn1.addEventListener("click", () => {
-    display.value += braketBtn1.value;
-});
-braketBtn2.addEventListener("click", () => {
-    display.value += braketBtn2.value;
-});
+
+
 
 equalBtn.addEventListener("click", () => {
-    display.value = calculate(display.value);
+    things_to_replace.clear();
+    expression = finalize(display.value);
+    display.value = calculate(expression);
 });
 backSpaceBtn.addEventListener("click", () => {
     display.value = display.value.slice(0, -1);
 });
 clearBtn.addEventListener("click", () => {
+    things_to_replace.clear();
     display.value = "";
 });
---
+++ test.js
@@ -0,0 +1,2 @@
+str = "sin(6) + cos(7) + sin(10)";
+str.matchAll(/sin/g);
